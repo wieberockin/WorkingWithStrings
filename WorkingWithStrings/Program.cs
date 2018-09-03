27818bf220a2e9b850c1e090ce38422fe25a025a
Ryan Wiebe <wieberockin@yahoo.com>
2018-09-03 00:10:11 -0600

    Random work with strings

--- WorkingWithStrings/Program.cs
+++ WorkingWithStrings/Program.cs
@@ -10,8 +10,64 @@ namespace WorkingWithStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //string myString = "My \"so-called\" life";
+            //string myString = "What if I need a \nnew line?";
+
+            //string myString = "Go to your c:\\ drive";
+            //string myString = @"Go to your c:\ drive";
+
+            //string myString = String.Format("{0} = {1}", "First", "Second");
+
+            //string myString = String.Format("{0:C}", 123.456);
+            //string myString = String.Format("{0:N}", 1234567890);
+
+            //string myString = String.Format("Percentage {0:P}", .123);
+
+            //string myString = String.Format("Phone Number: {0:(###) ###-####}", 123456789012);
+
+            //string myString = " That summer we took threes across the board  ";
+
+            //myString = myString.Substring(6, 14);
+            //string myString = myString.ToUpper();
+
+            //myString = myString.Replace(" ", "--");
+
+            // myString = myString.Remove(6, 14);
+
+            /*
+            myString = String.Format("Length before: {0} -- Length after: {1} ,",
+                myString.Length,
+                myString.Trim().Length);
+            */
+
+            /*
+            string myString = "";
+            for (int i = 0; i < 100; i++)
+            {
+                //This is inneficient because the .NET framework has to create a 
+                //temp string each time, and then it has to add the new string
+                //then it passes that value back as the named string, and 
+                //then it deleted the original string. This is how it handles an immutable
+                //string. 
+                //myString += "--" + i.ToString();
+            }
+            */
+
+
+            StringBuilder myString = new StringBuilder();
+
+            for (int i = 0; i < 100; i++)
+            {
+                myString.Append("--");
+                myString.Append(i);
+            }
+
+
+
+            Console.WriteLine(myString);
+            Console.ReadLine();
+            
+
         }
     }
 }
